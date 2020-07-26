25a5c3b3a63231a5d03aceec6b1fae5e5e41dbb9
Julian Krzefski <krzefski@heptacom.de>
2020-07-27 00:00:38 +0200

    SPBCC-11: Provide service discovery for webhook service

--- src/Bundle.php
+++ src/Bundle.php
@@ -2,6 +2,8 @@
 
 namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform;
 
+use Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Discovery\Strategy;
+use Heptacom\HeptaConnect\Portal\Base\Support\ServiceDiscovery;
 use Shopware\Core\Framework\Bundle as ShopwareBundle;
 
 class Bundle extends ShopwareBundle
@@ -9,5 +11,13 @@ class Bundle extends ShopwareBundle
     public function __construct()
     {
         $this->name = 'HeptaConnectBridgeShopwarePlatform';
+    }
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $this->container->get(Strategy::class);
+        ServiceDiscovery::appendStrategy(Strategy::class);
     }
 }
---
+++ src/Discovery/Strategy.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Discovery;
+
+use Heptacom\HeptaConnect\Portal\Base\Webhook\Contract\WebhookServiceInterface;
+use Http\Discovery\Strategy\DiscoveryStrategy;
+
+class Strategy implements DiscoveryStrategy
+{
+    private static WebhookServiceInterface $webhookService;
+
+    public static function setWebhookService(WebhookServiceInterface $webhookService): void
+    {
+        self::$webhookService = $webhookService;
+    }
+
+    public static function getCandidates($type)
+    {
+        return [
+            [
+                'condition' => fn () => is_a($type, WebhookServiceInterface::class, true),
+                'class' => fn () => self::$webhookService,
+            ],
+        ];
+    }
+}
