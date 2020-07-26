30965e7e12db416799192ae47b52480f7ccb1ff6
Julian Krzefski <krzefski@heptacom.de>
2020-07-26 21:26:42 +0200

    SPBCC-11: Use UriInterface in webhook url-provider

--- src/Webhook/UrlProvider.php
+++ src/Webhook/UrlProvider.php
@@ -3,6 +3,9 @@
 namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Webhook;
 
 use Heptacom\HeptaConnect\Core\Component\Webhook\Contract\UrlProviderInterface;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
@@ -11,16 +14,21 @@ class UrlProvider implements UrlProviderInterface
 {
     private RouterInterface $router;
 
+    private UriFactoryInterface $urlFactory;
+
     public function __construct(RouterInterface $router)
     {
         $this->router = $router;
+        $this->urlFactory = Psr17FactoryDiscovery::findUrlFactory();
     }
 
-    public function provide(): string
+    public function provide(): UriInterface
     {
-        return $this->router->generate('heptaconnect.api.webhook', [
+        $webhookUrl = $this->router->generate('heptaconnect.api.webhook', [
             'version' => 1,
             'id' => Uuid::uuid4()->getHex(),
-        ], UrlGeneratorInterface::ABSOLUTE_PATH);
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return $this->urlFactory->createUri($webhookUrl);
     }
 }
