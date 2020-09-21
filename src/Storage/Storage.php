50ad94e99f3e52eaabc88878a99cc6a5e56c9a38
Joshua Behrens <behrens@heptacom.de>
2020-09-21 02:04:40 +0200

    Extract MappingNodeRepositoryContract from Storage CONNECT-22

--- src/Storage/Storage.php
+++ src/Storage/Storage.php
@@ -3,120 +3,28 @@
 namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Storage;
 
 use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingInterface;
-use Heptacom\HeptaConnect\Portal\Base\Mapping\MappingCollection;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
-use Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageInterface;
 use Heptacom\HeptaConnect\Storage\Base\Exception\InvalidMappingNodeKeyException;
 use Heptacom\HeptaConnect\Storage\Base\Exception\InvalidPortalNodeKeyException;
-use Heptacom\HeptaConnect\Storage\Base\MappingNodeStructCollection;
 use Heptacom\HeptaConnect\Storage\Base\Support\StorageFallback;
-use Heptacom\HeptaConnect\Storage\ShopwareDal\Content\DatasetEntityType\DatasetEntityTypeCollection;
-use Heptacom\HeptaConnect\Storage\ShopwareDal\Content\DatasetEntityType\DatasetEntityTypeEntity;
-use Heptacom\HeptaConnect\Storage\ShopwareDal\Content\Mapping\MappingNodeEntity;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\MappingNodeStorageKey;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Uuid\Uuid;
 
 class Storage extends StorageFallback implements StorageInterface
 {
-    private EntityRepositoryInterface $mappingNodes;
-
-    private EntityRepositoryInterface $datasetEntityTypes;
-
     private EntityRepositoryInterface $mappings;
 
     private EntityRepositoryInterface $errorMessages;
 
-    public function __construct(
-        EntityRepositoryInterface $datasetEntityTypes,
-        EntityRepositoryInterface $mappingNodes,
-        EntityRepositoryInterface $mappings,
-        EntityRepositoryInterface $errorMessages
-    ) {
-        $this->datasetEntityTypes = $datasetEntityTypes;
-        $this->mappingNodes = $mappingNodes;
+    public function __construct(EntityRepositoryInterface $mappings, EntityRepositoryInterface $errorMessages)
+    {
         $this->mappings = $mappings;
         $this->errorMessages = $errorMessages;
-    }
-
-    public function getMappingNode(string $datasetEntityClassName, PortalNodeKeyInterface $portalNodeKey, string $externalId): ?MappingNodeStructInterface
-    {
-        $context = Context::createDefaultContext();
-
-        if (!$portalNodeKey instanceof PortalNodeStorageKey) {
-            throw new \Exception();
-        }
-
-        $criteria = (new Criteria())
-            ->addFilter(
-                new EqualsFilter('deletedAt', null),
-                new EqualsFilter('type.type', $datasetEntityClassName),
-                new EqualsFilter('mappings.deletedAt', null),
-                new EqualsFilter('mappings.externalId', $externalId),
-                new EqualsFilter('mappings.portalNode.deletedAt', null),
-                new EqualsFilter('mappings.portalNode.id', $portalNodeKey->getUuid()),
-            )
-            ->setLimit(1)
-        ;
-
-        $mappingNode = $this->mappingNodes->search($criteria, $context)->first();
-
-        if ($mappingNode instanceof MappingNodeStructInterface) {
-            return $mappingNode;
-        }
-
-        return null;
-    }
-
-    public function createMappingNodes(array $datasetEntityClassNames, PortalNodeKeyInterface $portalNodeKey): MappingNodeStructCollection
-    {
-        if (\count($datasetEntityClassNames) === 0) {
-            return new MappingNodeStructCollection();
-        }
-
-        if (!$portalNodeKey instanceof PortalNodeStorageKey) {
-            return parent::createMappingNodes($datasetEntityClassNames, $portalNodeKey);
-        }
-
-        $context = Context::createDefaultContext();
-        $typesToCheck = \array_unique($datasetEntityClassNames);
-        $typeIds = $this->getIdsForDatasetEntityType($typesToCheck, $context);
-
-        $result = [];
-        $mappingNodeInsert = [];
-
-        foreach ($datasetEntityClassNames as $datasetEntityClassNameKey => $datasetEntityClassName) {
-            $mappingId = Uuid::randomHex();
-            $mappingNodeInsert[] = [
-                'id' => $mappingId,
-                'originPortalNodeId' => $portalNodeKey->getUuid(),
-                'typeId' => $typeIds[$datasetEntityClassName],
-            ];
-
-            $type = new DatasetEntityTypeEntity();
-            $type->setCreatedAt(new \DateTime());
-            $type->setType($datasetEntityClassName);
-            $type->setId($typeIds[$datasetEntityClassName]);
-            $mapping = (new MappingNodeEntity())
-                ->setType($type)
-                ->setTypeId($type->getId())
-                ->setOriginPortalNodeId($portalNodeKey->getUuid())
-            ;
-            $mapping->setCreatedAt(new \DateTime());
-            $mapping->setId($mappingId);
-            $result[$datasetEntityClassNameKey] = $mapping;
-        }
-
-        $this->mappingNodes->create($mappingNodeInsert, $context);
-
-        return new MappingNodeStructCollection($result);
     }
 
     public function addMappingException(MappingInterface $mapping, \Throwable $throwable): void
@@ -191,37 +99,6 @@ class Storage extends StorageFallback implements StorageInterface
         );
 
         return $this->mappings->searchIds($criteria, $context)->firstId();
-    }
-
-    /**
-     * @psalm-param array<array-key, class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>> $types
-     * @psalm-return array<class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>, string>
-     */
-    private function getIdsForDatasetEntityType(array $types, Context $context): array
-    {
-        $datasetEntityCriteria = new Criteria();
-        $datasetEntityCriteria->addFilter(new EqualsAnyFilter('type', $types));
-        /** @var DatasetEntityTypeCollection $datasetTypeEntities */
-        $datasetTypeEntities = $this->datasetEntityTypes->search($datasetEntityCriteria, $context)->getEntities();
-        $typeIds = $datasetTypeEntities->groupByType();
-        $datasetTypeInsert = [];
-
-        foreach ($types as $className) {
-            if (!\array_key_exists($className, $typeIds)) {
-                $id = Uuid::randomHex();
-                $datasetTypeInsert[] = [
-                    'id' => $id,
-                    'type' => $className,
-                ];
-                $typeIds[$className] = $id;
-            }
-        }
-
-        if (\count($datasetTypeInsert) > 0) {
-            $this->datasetEntityTypes->create($datasetTypeInsert, $context);
-        }
-
-        return $typeIds;
     }
 
     /**
--- test/StorageTest.php
+++ test/StorageTest.php
@@ -3,19 +3,13 @@
 namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Test;
 
 use Doctrine\DBAL\Connection;
-use Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Storage\Storage;
-use Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Test\Fixture\FooBarDatasetEntity;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
-use Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
 
 /**
  * @covers \Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Bundle
  * @covers \Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Storage\AbstractStorageKey
- * @covers \Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Storage\Storage
  * @covers \Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey
  */
 class StorageTest extends TestCase
@@ -38,29 +32,6 @@ class StorageTest extends TestCase
         $connection = $this->kernel->getContainer()->get(Connection::class);
         $connection->rollBack();
         $this->kernel->shutdown();
-    }
-
-    public function testCreateMappingNodeByType(): void
-    {
-        /** @var DefinitionInstanceRegistry $definitionRegistry */
-        $definitionRegistry = $this->kernel->getContainer()->get(DefinitionInstanceRegistry::class);
-        $storage = new Storage(
-            $definitionRegistry->getRepository('heptaconnect_dataset_entity_type'),
-            $definitionRegistry->getRepository('heptaconnect_mapping_node'),
-            $definitionRegistry->getRepository('heptaconnect_mapping'),
-            $definitionRegistry->getRepository('heptaconnect_error_message')
-        );
-
-        $portalNodes = $definitionRegistry->getRepository('heptaconnect_portal_node');
-        $portalNodeId = new PortalNodeStorageKey('bf540cbefe774a88addc8c33b58dae66');
-        $portalNodes->create([['id' => $portalNodeId->getUuid()]], Context::createDefaultContext());
-
-        /** @var MappingNodeStructInterface $node */
-        $node = $storage->createMappingNodes([FooBarDatasetEntity::class], $portalNodeId)->first();
-        static::assertEquals(FooBarDatasetEntity::class, $node->getDatasetEntityClassName());
-
-        $node = $storage->createMappingNodes(['key' => FooBarDatasetEntity::class], $portalNodeId)->offsetGet('key');
-        static::assertEquals(FooBarDatasetEntity::class, $node->getDatasetEntityClassName());
     }
 
     public function testKeyComparison(): void
