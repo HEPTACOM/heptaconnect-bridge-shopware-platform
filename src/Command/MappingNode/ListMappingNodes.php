b99a705bb0da39facfe86146f848dda228b0708c
Joshua Behrens <behrens@heptacom.de>
2020-09-22 10:58:30 +0200

    Add command to list existing mapping nodes CONNECT-18

---
+++ src/Command/MappingNode/ListMappingNodes.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Command\MappingNode;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\MappingRepositoryContract;
+use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ListMappingNodes extends Command
+{
+    protected static $defaultName = 'heptaconnect:mapping-node:list';
+
+    private MappingRepositoryContract $mappingRepository;
+
+    private StorageKeyGeneratorContract $storageKeyGenerator;
+
+    public function __construct(
+        MappingRepositoryContract $mappingRepository,
+        StorageKeyGeneratorContract $storageKeyGenerator
+    ) {
+        parent::__construct();
+        $this->mappingRepository = $mappingRepository;
+        $this->storageKeyGenerator = $storageKeyGenerator;
+    }
+
+    protected function configure()
+    {
+        $this->addArgument('dataset-entity-class', InputArgument::REQUIRED)
+            ->addArgument('portal-node-key', InputArgument::REQUIRED);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $datasetEntityClass = (string) $input->getArgument('dataset-entity-class');
+        $portalNodeKey = $this->storageKeyGenerator->deserialize((string) $input->getArgument('portal-node-key'));
+
+        if (!\is_a($datasetEntityClass, DatasetEntityInterface::class, true)) {
+            $io->error('The provided dataset entity class does not implement the DatasetEntityInterface.');
+
+            return 1;
+        }
+
+        if (!\is_a($portalNodeKey, PortalNodeKeyInterface::class, false)) {
+            $io->error('The provided portal-node-key is not a PortalNodeKeyInterface.');
+
+            return 2;
+        }
+
+        $rows = [];
+        $iterator = $this->mappingRepository->listByPortalNodeAndType($portalNodeKey, $datasetEntityClass);
+
+        foreach ($iterator as $mappingKey) {
+            $mapping = $this->mappingRepository->read($mappingKey);
+            $rows[] = [
+                'mapping-node-id' => $this->storageKeyGenerator->serialize($mapping->getMappingNodeKey()),
+                'external-id' => $mapping->getExternalId(),
+            ];
+        }
+
+        if (empty($rows)) {
+            $io->note('There are no mapping nodes of the selected portal.');
+
+            return 0;
+        }
+
+        $io->table(\array_keys(\current($rows)), $rows);
+
+        return 0;
+    }
+}
