2766c57b3af75728cbb472c3af5d809c5914e173
Joshua Behrens <behrens@heptacom.de>
2021-05-22 18:35:14 +0200

    Add logger output to every command

---
+++ src/Support/CommandsPrintLogsSubscriber.php
@@ -0,0 +1,129 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Support;
+
+use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CommandsPrintLogsSubscriber implements EventSubscriberInterface
+{
+    public const LOGGER_STREAM = 'php://stderr';
+
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ConsoleCommandEvent::class => 'onPreCommandRun',
+            ConsoleEvents::COMMAND => 'onPreCommandRun',
+            ConsoleTerminateEvent::class => 'onPostCommandRun',
+            ConsoleEvents::TERMINATE => 'onPostCommandRun',
+        ];
+    }
+
+    public function onPreCommandRun(ConsoleCommandEvent $event): void
+    {
+        $output = $event->getOutput();
+
+        if ($output->isQuiet()) {
+            return;
+        }
+
+        if (!$this->logger instanceof Logger) {
+            return;
+        }
+
+        $logLevel = $this->getLogLevelForOutput($output);
+
+        if ($this->hasLoggerAlreadyHandler($logLevel)) {
+            return;
+        }
+
+        $this->logger->pushHandler(new StreamHandler(self::LOGGER_STREAM, $logLevel));
+    }
+
+    public function onPostCommandRun(ConsoleTerminateEvent $event): void
+    {
+        if (!$this->logger instanceof Logger) {
+            return;
+        }
+
+        $this->logger->setHandlers(\array_filter(
+            $this->logger->getHandlers(),
+            $this->removeErrorStreamHandlerOnce($this->getLogLevelForOutput($event->getOutput()))
+        ));
+    }
+
+    protected function removeErrorStreamHandlerOnce(int $logLevel): callable
+    {
+        $removedOnce = false;
+
+        return static function (HandlerInterface $h) use (&$removedOnce, $logLevel): bool {
+            if (!$this->isErrorStreamHandler($h, $logLevel)) {
+                return false;
+            }
+
+            if ($removedOnce) {
+                return false;
+            }
+
+            $removedOnce = true;
+
+            return true;
+        };
+    }
+
+    protected function getLogLevelForOutput(OutputInterface $output): int
+    {
+        if ($output->isDebug()) {
+            return Logger::DEBUG;
+        }
+
+        if ($output->isVeryVerbose()) {
+            return Logger::INFO;
+        }
+
+        return Logger::WARNING;
+    }
+
+    protected function hasLoggerAlreadyHandler(int $logLevel): bool
+    {
+        foreach ($this->logger->getHandlers() as $handler) {
+            if ($this->isErrorStreamHandler($handler, $logLevel)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function isErrorStreamHandler(HandlerInterface $handler, int $logLevel): bool
+    {
+        if (!$handler instanceof StreamHandler) {
+            return false;
+        }
+
+        if ($handler->getLevel() !== $logLevel) {
+            return false;
+        }
+
+        if ($handler->getUrl() !== self::LOGGER_STREAM) {
+            return false;
+        }
+
+        return true;
+    }
+}
