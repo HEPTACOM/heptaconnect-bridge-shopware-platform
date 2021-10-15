20c1afb7220790944a7a4a29848d22747ea94bdf
Joshua Behrens <behrens@heptacom.de>
2021-10-16 01:18:00 +0200

    Use job keys instead of ids to make use of storage abstraction layer
    CONNECT-162

--- src/Command/Job/Rerun.php
+++ src/Command/Job/Rerun.php
@@ -6,14 +6,16 @@ namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Command\Job;
 use Heptacom\HeptaConnect\Core\Job\Contract\DelegatingJobActorContract;
 use Heptacom\HeptaConnect\Core\Job\JobData;
 use Heptacom\HeptaConnect\Core\Job\JobDataCollection;
+use Heptacom\HeptaConnect\Storage\Base\Contract\JobKeyInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\JobPayloadRepositoryContract;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\JobRepositoryContract;
+use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
 use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
-use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\JobStorageKey;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class Rerun extends Command
 {
@@ -25,30 +27,38 @@ class Rerun extends Command
 
     private DelegatingJobActorContract $jobActor;
 
+    private StorageKeyGeneratorContract $storageKeyGenerator;
+
     public function __construct(
         JobRepositoryContract $jobRepository,
         JobPayloadRepositoryContract $jobPayloadRepository,
-        DelegatingJobActorContract $jobActor
+        DelegatingJobActorContract $jobActor,
+        StorageKeyGeneratorContract $storageKeyGenerator
     ) {
         parent::__construct();
         $this->jobPayloadRepository = $jobPayloadRepository;
         $this->jobRepository = $jobRepository;
         $this->jobActor = $jobActor;
+        $this->storageKeyGenerator = $storageKeyGenerator;
     }
 
     protected function configure(): void
     {
-        $this->addArgument('jobId', InputArgument::REQUIRED, 'The id of the job');
+        $this->addArgument('job-key', InputArgument::REQUIRED, 'The key of the job');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $inputJobId = $input->getArgument('jobId');
-        // For easier copy/paste from database id
-        $jobId = \strtolower(\str_replace(' ', '', $inputJobId));
+        $io = new SymfonyStyle($input, $output);
+        $jobKey = $this->storageKeyGenerator->deserialize((string) $input->getArgument('job-key'));
+
+        if (!\is_a($jobKey, JobKeyInterface::class, true)) {
+            $io->error('The provided job-key is not a JobKeyInterface.');
+
+            return 1;
+        }
 
         try {
-            $jobKey = new JobStorageKey($jobId);
             $job = $this->jobRepository->get($jobKey);
             $payloadKey = $job->getPayloadKey();
             $payload = $payloadKey !== null ? $this->jobPayloadRepository->get($payloadKey) : null;
