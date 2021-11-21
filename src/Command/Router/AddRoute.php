682542581475c373422f615c72fadb03d97fcc2b
Joshua Behrens <behrens@heptacom.de>
2021-11-21 22:34:36 +0100

    Rename RouteGetResult::getTargetKey to RouteGetResult::getTargetPortalNodeKey

--- src/Command/Router/AddRoute.php
+++ src/Command/Router/AddRoute.php
@@ -109,7 +109,7 @@ class AddRoute extends Command
                 'id' => $this->storageKeyGenerator->serialize($route->getRouteKey()),
                 'type' => $route->getEntityType(),
                 'source' => $this->storageKeyGenerator->serialize($route->getSourceKey()),
-                'target' => $this->storageKeyGenerator->serialize($route->getTargetKey()),
+                'target' => $this->storageKeyGenerator->serialize($route->getTargetPortalNodeKey()),
                 'capabilities' => \implode(', ', $capabilities),
             ];
         }
