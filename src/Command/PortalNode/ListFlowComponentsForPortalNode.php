a9101c13116c7005715c46964de337e89cb0d69b
Michael Benz <benz@heptacom.de>
2021-11-22 19:57:00 +0100

    Changed command to use origin-method of stacks

--- src/Command/PortalNode/ListFlowComponentsForPortalNode.php
+++ src/Command/PortalNode/ListFlowComponentsForPortalNode.php
@@ -3,24 +3,23 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Bridge\ShopwarePlatform\Command\PortalNode;
 
-use Heptacom\HeptaConnect\Core\Job\Contract\JobContract;
-use Heptacom\HeptaConnect\Core\Job\Type\Emission;
-use Heptacom\HeptaConnect\Core\Job\Type\Exploration;
-use Heptacom\HeptaConnect\Core\Job\Type\Reception;
-use Heptacom\HeptaConnect\Core\Portal\PortalStackServiceContainerFactory;
+use Heptacom\HeptaConnect\Core\Emission\Contract\EmitterStackBuilderFactoryInterface;
+use Heptacom\HeptaConnect\Core\Exploration\ExplorerStackBuilderFactory;
+use Heptacom\HeptaConnect\Core\Reception\ReceiverStackBuilderFactory;
+use Heptacom\HeptaConnect\Core\Web\Http\Contract\HttpHandlerStackBuilderFactoryInterface;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
-use Heptacom\HeptaConnect\Portal\Base\Builder\ShorthandFlowComponent;
-use Heptacom\HeptaConnect\Portal\Base\Emission\EmitterCollection;
-use Heptacom\HeptaConnect\Portal\Base\Exploration\ExplorerCollection;
-use Heptacom\HeptaConnect\Portal\Base\Reception\ReceiverCollection;
+use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterContract;
+use Heptacom\HeptaConnect\Portal\Base\Emission\EmitterStack;
+use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExplorerContract;
+use Heptacom\HeptaConnect\Portal\Base\Exploration\ExplorerStack;
+use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverContract;
+use Heptacom\HeptaConnect\Portal\Base\Reception\ReceiverStack;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface;
-use Heptacom\HeptaConnect\Portal\Base\Support\Contract\FlowComponentContract;
-use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\PortalNodeRepositoryContract;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpHandlerContract;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\HttpHandlerStack;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
 use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
-use Opis\Closure\ReflectionClosure;
-use ReflectionClass;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -33,32 +32,34 @@ class ListFlowComponentsForPortalNode extends Command
 
     private StorageKeyGeneratorContract $storageKeyGenerator;
 
-    private PortalStackServiceContainerFactory $portalStackServiceContainerFactory;
+    private ExplorerStackBuilderFactory $explorerStackBuilderFactory;
 
-    private PortalNodeRepositoryContract $portalNodeRepository;
+    private EmitterStackBuilderFactoryInterface $emitterStackBuilderFactory;
 
-    private array $jobToCollectionMap = [
-        Exploration::class => ExplorerCollection::class,
-        Emission::class => EmitterCollection::class,
-        Reception::class => ReceiverCollection::class,
-    ];
+    private ReceiverStackBuilderFactory $receiverStackBuilderFactory;
+
+    private HttpHandlerStackBuilderFactoryInterface $httpHandlerStackBuilderFactory;
 
     public function __construct(
         StorageKeyGeneratorContract $storageKeyGenerator,
-        PortalStackServiceContainerFactory $portalStackServiceContainerFactory,
-        PortalNodeRepositoryContract $portalNodeRepository
+        ExplorerStackBuilderFactory $explorerStackBuilderFactory,
+        EmitterStackBuilderFactoryInterface $emitterStackBuilderFactory,
+        ReceiverStackBuilderFactory $receiverStackBuilderFactory,
+        HttpHandlerStackBuilderFactoryInterface $httpHandlerStackBuilderFactory
     ) {
         parent::__construct();
         $this->storageKeyGenerator = $storageKeyGenerator;
-        $this->portalStackServiceContainerFactory = $portalStackServiceContainerFactory;
-        $this->portalNodeRepository = $portalNodeRepository;
+        $this->explorerStackBuilderFactory = $explorerStackBuilderFactory;
+        $this->emitterStackBuilderFactory = $emitterStackBuilderFactory;
+        $this->receiverStackBuilderFactory = $receiverStackBuilderFactory;
+        $this->httpHandlerStackBuilderFactory = $httpHandlerStackBuilderFactory;
     }
 
     protected function configure()
     {
         $this->addArgument('portal-node-key', InputArgument::REQUIRED);
-        $this->addArgument('type', InputArgument::REQUIRED);
-        $this->addArgument('job', InputArgument::REQUIRED);
+        $this->addArgument('entity-type', InputArgument::REQUIRED);
+        $this->addArgument('flow-component-contract', InputArgument::REQUIRED);
         $this->addOption('pretty', InputArgument::OPTIONAL);
     }
 
@@ -78,82 +79,93 @@ class ListFlowComponentsForPortalNode extends Command
             return 1;
         }
 
-        $type = $input->getArgument('type');
+        $entityType = $input->getArgument('entity-type');
 
-        if (!\is_a($type, DatasetEntityContract::class, true)) {
+        if (!\is_a($entityType, DatasetEntityContract::class, true)) {
             $io->error('The specified type does not implement the DatasetEntityContract.');
 
             return 1;
         }
 
-        $job = $input->getArgument('job');
-
-        if (!\is_a($job, JobContract::class, true)) {
-            $io->error('The specified type does not implement the JobContract.');
-
-            return 1;
-        }
-
-        $container = $this->portalStackServiceContainerFactory->create($portalNodeKey);
-        $flowComponents = $container->get($this->jobToCollectionMap[$job]);
-        $flowComponentDecorators = $container->get($this->jobToCollectionMap[$job].'.decorator');
-        $flowComponents->push($flowComponentDecorators);
-        $portalName = $this->portalNodeRepository->read($portalNodeKey);
-
-        /**
-         * @var FlowComponentContract $flowComponent
-         */
+        $flowComponentContract = $input->getArgument('flow-component-contract');
         $flowComponentDescriptions = [];
-        foreach ($flowComponents->getIterator() as $flowComponent) {
-            if ($flowComponent->supports() === $type) {
-                if ($flowComponent instanceof ShorthandFlowComponent) {
-                    foreach ($flowComponent->getMethods() as $methodName => $method) {
-                        /**
-                         * @var ReflectionClosure $reflectedMethod
-                         */
-                        $reflectedMethod = $method->getReflector();
-                        $method = [
-                            'method' => $methodName,
-                            'start_line' => $reflectedMethod->getStartLine(),
-                            'end_line' => $reflectedMethod->getEndLine(),
-                        ];
-                        $flowComponentDescription = [
-                            'file_name' => $reflectedMethod->getFileName(),
-                            'type' => 'shorthand_implementation',
-                            'methods' => [$method],
-                        ];
-                        $flowComponentDescriptions[] = $flowComponentDescription;
-                    }
-                } else {
-                    $reflectedFlowComponent = new ReflectionClass($flowComponent);
-                    $methodDescriptions = [];
-                    foreach ($reflectedFlowComponent->getMethods() as $reflectedMethod) {
-                        if ($reflectedMethod->getDeclaringClass()->getName() === $reflectedFlowComponent->getName()) {
-                            $method = [
-                                'method' => $reflectedMethod->getName(),
-                                'start_line' => $reflectedMethod->getStartLine(),
-                                'end_line' => $reflectedMethod->getEndLine(),
-                            ];
-                            $methodDescriptions[] = $method;
-                        }
-                    }
-                    $flowComponentDescription = [
-                        'file_name' => $reflectedFlowComponent->getFileName(),
-                        'type' => 'full_implementation',
-                        'methods' => $methodDescriptions,
-                    ];
-                    $flowComponentDescriptions[] = $flowComponentDescription;
-                }
-            }
+        switch ($flowComponentContract) {
+            case ExplorerContract::class:
+                $flowComponentDescriptions = $this->getExplorerImplementations($portalNodeKey, $entityType);
+                break;
+            case ReceiverContract::class:
+                $flowComponentDescriptions = $this->getReceiverImplementations($portalNodeKey, $entityType);
+                break;
+            case EmitterContract::class:
+                $flowComponentDescriptions = $this->getEmitterImplementations($portalNodeKey, $entityType);
+                break;
+            case HttpHandlerContract::class:
+                $flowComponentDescriptions = $this->getHttpHandlerImplementations($portalNodeKey, $entityType);
+                break;
+            default:
+                $io->error('The specified flow-component-contract does not exist.');
         }
-        $description = [
-            'portal' => $portalName,
-            'job' => $job,
-            'flowComponents' => $flowComponentDescriptions,
-        ];
+
         $flags = $input->getOption('pretty') ? \JSON_PRETTY_PRINT : 0;
-        $io->writeln(\json_encode($description, $flags));
+        $io->writeln(\json_encode($flowComponentDescriptions, $flags));
 
         return 0;
+    }
+
+    private function getExplorerImplementations(PortalNodeKeyInterface $portalNodeKey, string $entityType): array
+    {
+        $explorerStackBuilder = $this->explorerStackBuilderFactory
+            ->createExplorerStackBuilder($portalNodeKey, $entityType)
+            ->pushSource()
+            ->pushDecorators();
+
+        /**
+         * @var ExplorerStack $explorerStack
+         */
+        $explorerStack = $explorerStackBuilder->build();
+
+        return $explorerStack->listOrigins();
+    }
+
+    private function getReceiverImplementations(PortalNodeKeyInterface $portalNodeKey, string $entityType): array
+    {
+        $receiverStackBuilder = $this->receiverStackBuilderFactory
+            ->createReceiverStackBuilder($portalNodeKey, $entityType)
+            ->pushSource()
+            ->pushDecorators();
+        /**
+         * @var ReceiverStack $receiverStack
+         */
+        $receiverStack = $receiverStackBuilder->build();
+
+        return $receiverStack->listOrigins();
+    }
+
+    private function getEmitterImplementations(PortalNodeKeyInterface $portalNodeKey, string $entityType): array
+    {
+        $emitterStackBuilder = $this->emitterStackBuilderFactory
+            ->createEmitterStackBuilder($portalNodeKey, $entityType)
+            ->pushSource()
+            ->pushDecorators();
+        /**
+         * @var EmitterStack $emitterStack
+         */
+        $emitterStack = $emitterStackBuilder->build();
+
+        return $emitterStack->listOrigins();
+    }
+
+    private function getHttpHandlerImplementations(PortalNodeKeyInterface $portalNodeKey, string $entityType): array
+    {
+        $httpHandlerStackBuilder = $this->httpHandlerStackBuilderFactory
+            ->createHttpHandlerStackBuilder($portalNodeKey, $entityType)
+            ->pushSource()
+            ->pushDecorators();
+        /**
+         * @var HttpHandlerStack $httpStack
+         */
+        $httpStack = $httpHandlerStackBuilder->build();
+
+        return $httpStack->listOrigins();
     }
 }
